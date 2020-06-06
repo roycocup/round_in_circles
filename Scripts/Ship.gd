24b51c15936436b6ea7323e5e5aeee6cd612c8ca
Rodrigo Dias <rodrigo.pitta@gmail.com>
2020-06-06 17:37:14 +0100

    rotating

--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -1,29 +1,31 @@
-extends Node2D
+extends KinematicBody2D
 
 onready var world = get_tree().get_root().get_node("World")
+onready var point = world.get_node("Center").transform.origin
 
 var look_at_point = Vector2()
-onready var point = world.get_node("Center").transform.origin
+
+export var speed = 100
 
 func _ready():
 	pass
 
 func _process(delta):	
-	look_at(point)
-	rotate(deg2rad(90))
-	
+	pass
 	
 func _input(event):
 	if event is InputEventKey:
+		var velocity = Vector2()
+		look_at(point)
+		rotate(deg2rad(90))
 		if Input.is_action_pressed("ui_left"):
-			position.x -= 5
-			position.y -= 5
+			velocity = Vector2(-1, 0).rotated(rotation) * speed
 		if Input.is_action_pressed("ui_right"):
-			position.x += 5
-			position.y += 5			
+			velocity = Vector2(1, 0).rotated(rotation) * speed
+			
+		move_and_slide(velocity)
 
 func _draw():
-	$Sprite.visible = true
 	pass
 		
 		
